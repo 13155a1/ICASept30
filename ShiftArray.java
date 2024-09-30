0675a98f2645361bf92175e19d43adb67371ec7f
13155a1 <ppy040@gmail.com>
2024-09-30 12:07:48 -0700

    Fixed the bug

--- ShiftArray.java
+++ ShiftArray.java
@@ -3,7 +3,7 @@ public class ShiftArray {
     // A method to shift all the items of "arr" to the right 
 	// by "shift" number of places (with wraparound).
 	// Example: {3, 4, 5, 6} with a shift of 1 becomes {6, 3, 4, 5} 
-	public static void shiftBy(int[] arr, int shift) {
+	public static int[] shiftBy(int[] arr, int shift) {
 
 		// If shift is larger than the array, this expression
 		// finds an equivalent shift amount. If shift is less
@@ -16,9 +16,19 @@ public class ShiftArray {
 		for(int i = 0; i < arr.length; i++){
 			int index = (i+shift) % arr.length;
 			newArray[index] = arr[i];
+
+			// System.out.println("i: " + i);
+			// System.out.println("index: " + index);
+			// System.out.println("arr[i]: " + arr[i]);
+			// System.out.println();
 		}
 
-		arr = newArray;
+		// for(int i : newArray){
+		// 	System.out.print(i + " ");
+		// }
+		// System.out.println();
+
+		return newArray;
 	}
 
 
@@ -30,8 +40,8 @@ public class ShiftArray {
 				System.out.println("Sorry, shift about must be positive.");
 				return;
 			}
-			shiftBy(a, shiftAmount);
-			for(int i : a){
+			int[] newArray = shiftBy(a, shiftAmount);
+			for(int i : newArray){
 				System.out.print(i + " ");
 			}
 		}catch(Exception e){
